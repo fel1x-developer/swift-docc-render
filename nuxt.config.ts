47704c159fa3cee7a08eb3bcf1695c5101444288
Minsoo Choo <minsoochoo0122@proton.me>
2025-02-02 22:46:28 -0500

    Add Nuxt modules
     - ESLint
     - TailwindCSS
     - Test Utils
     - Typescript
     - i18n
     - Icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,29 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+  i18n: {
+    locales: [
+      { code: 'en', language: 'en-US' },
+      { code: 'ja', language: 'ja-JP' },
+      { code: 'ko', language: 'ko-KR' },
+      { code: 'zh', language: 'zh-CN' },
+    ],
+    defaultLocale: 'en',
+  },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/test-utils/module',
+    '@nuxtjs/i18n',
+    '@nuxtjs/tailwindcss',
+  ],
   ssr: false,
+  typescript: {
+    typeCheck: true,
+  },
 })
